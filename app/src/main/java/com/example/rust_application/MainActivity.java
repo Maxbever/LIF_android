6a29fe835bf50a3c5421f47d935ca33a27d07b5e
Maxime Bever <36727317+Maxbever@users.noreply.github.com>
2022-06-03 13:09:17 +0200

    Add location

--- app/src/main/java/com/example/rust_application/MainActivity.java
+++ app/src/main/java/com/example/rust_application/MainActivity.java
@@ -1,33 +1,85 @@
 package com.example.rust_application;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient fusedLocationClient;
 
-    private static final String TAG = "MainActivity";
-
-    // Used to load the 'rust' library on application startup.
     static {
         System.loadLibrary("lif_android");
     }
 
     TextView textView;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.sample_text);
 
-        Thread thread = new Thread(){
-            public void run(){
+        Thread thread = new Thread() {
+            public void run() {
                 main();
             }
         };
 
         thread.start();
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityResultLauncher<String[]> locationPermissionRequest =
+                    registerForActivityResult(new ActivityResultContracts
+                                    .RequestMultiplePermissions(), result -> {
+                                Boolean fineLocationGranted = result.getOrDefault(
+                                        Manifest.permission.ACCESS_FINE_LOCATION, false);
+                                Boolean coarseLocationGranted = result.getOrDefault(
+                                        Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                                if (fineLocationGranted != null && fineLocationGranted) {
+                                    // Precise location access granted.
+                                } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                    // Only approximate location access granted.
+                                } else {
+                                    // No location access granted.
+                                }
+                            }
+                    );
+
+            // Before you perform the actual permission request, check whether your app
+            // already has the permissions, and whether your app needs to show a permission
+            // rationale dialog. For more details, see Request permissions.
+            locationPermissionRequest.launch(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Log.println(Log.VERBOSE, "LOCA2",location.toString());
+                        }
+                    }
+                });
     }
 
     /**
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,12 +1,16 @@
 use android_logger::Config;
-use log::Level;
+use jni::JNIEnv;
+use jni::objects::{JClass, JObject};
+use log::{Level, trace};
 use rustupolis_server::repository::Repository;
 use rustupolis_server::server::{Protocol, Server};
 use rustupolis_server::server_launcher::ServerLauncher;
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "C" fn Java_com_example_rust_1application_MainActivity_main() {
+pub extern "C" fn Java_com_example_rust_1application_MainActivity_main(    _env: JNIEnv,
+                                                                           _class: JClass,
+                                                                           _callback: JObject) {
     android_logger::init_once(Config::default().with_min_level(Level::Trace));
 
     let ip_address = String::from("192.168.1.139");
@@ -14,6 +18,7 @@ pub extern "C" fn Java_com_example_rust_1application_MainActivity_main() {
     let port_udp = String::from("9001");
 
     let repository = Repository::new("admin");
+    repository.add_tuple_space(String::from("GPS_DATA"),vec![String::from("admin")]);
 
     let server_tcp = Server::new(Protocol::TCP, &ip_address, &port_tcp, &repository);
     let server_udp = Server::new(Protocol::UDP, &ip_address, &port_udp, &repository);
